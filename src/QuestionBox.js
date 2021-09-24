1ca33293fbbfb5ef9957be29e079772e0fb4edea
PJongu <69169366+PJongu@users.noreply.github.com>
2021-09-23 21:34:07 -0400

    Styled things

--- src/QuestionBox.js
+++ src/QuestionBox.js
@@ -1,9 +1,14 @@
+import "./QuestionBox.css"
+
 function QuestionBox(){
-    <div class="qb-surround">
+    return <div class="qb-surround">
         <section class="qb-section">
             <h1 class="qb-question-title">What is joe?</h1>
             <section class="qb-question-choices">
-
+            <Answer text="A js framework"/>
+            <Answer text="joseph"/>
+            <Answer text="mother"/>
+            <Answer text="lmao"/>
             </section>
         </section>
     </div>
